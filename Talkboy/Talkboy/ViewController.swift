276e502369440ef8820f471aaa7878453013c813
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-11-24 19:56:00 -0600

    Initial commit

---
+++ Talkboy/Talkboy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Talkboy
+//
+//  Created by Kevin Mudiandambo on 11/24/17.
+//  Copyright © 2017 Kevin Mudiandambo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
