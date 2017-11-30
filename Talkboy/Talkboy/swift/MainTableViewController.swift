62c36e13ab546efb3e1ae5865a482e611f6c0ae4
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-11-29 22:26:44 -0600

    Can now delete the sounds added to tableview

--- Talkboy/Talkboy/swift/MainTableViewController.swift
+++ Talkboy/Talkboy/swift/MainTableViewController.swift
@@ -56,8 +56,17 @@ class MainTableViewController: UITableViewController {
     
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let sound = sounds[indexPath.row]
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                context.delete(sound)
+                loadItemsFromCoreData()
+            }
+        }
     }
     
 
@@ -69,5 +78,7 @@ class MainTableViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    
 
 }
